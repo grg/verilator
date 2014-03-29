830578982bf881d7db407eae222e2a4fde99ec41
Glen Gibb <glen.gibb@alumni.stanford.edu>
2014-03-28 17:47:23 -0700

    AstStream: replace dtype with const in slice size
    In a stream operation, replace a basic data type used for the slice size
    with a constant.

--- src/V3Width.cpp
+++ src/V3Width.cpp
@@ -374,10 +374,16 @@ private:
 	    checkCvtUS(nodep->rhsp());
 	    V3Const::constifyParamsEdit(nodep->rhsp()); // rhsp may change
 	    AstConst* constp = nodep->rhsp()->castConst();
-	    AstBasicDType* bdtypep = nodep->rhsp()->castBasicDType();
-	    if (!constp && !bdtypep) { nodep->v3error("Slice size isn't a constant or basic data type."); return; }
-	    uint32_t sliceSize = constp ? constp->toUInt() : bdtypep->width();
-	    if (!sliceSize) { nodep->v3error("Slice size cannot be zero."); return; }
+	    AstBasicDType* basicp = nodep->rhsp()->castBasicDType();
+	    if (!constp && !basicp) { nodep->v3error("Slice size isn't a constant or basic data type."); return; }
+            if (basicp) { // Convert data type to a constant size
+                AstConst* newp = new AstConst(basicp->fileline(), basicp->width());
+                nodep->rhsp()->replaceWith(newp);
+                pushDeletep(basicp);
+            } else {
+                uint32_t sliceSize = constp->toUInt();
+                if (!sliceSize) { nodep->v3error("Slice size cannot be zero."); return; }
+            }
 	    nodep->dtypeSetLogicSized((nodep->lhsp()->width()),
 				      (nodep->lhsp()->widthMin()),
 				      AstNumeric::UNSIGNED);
